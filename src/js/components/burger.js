23f4acf2c3adb3bbee0a01aa51a6d92163f5ed19
Vetosy <xigga11@gmail.com>
2024-01-18 17:41:22 +0300

    add header

--- src/js/components/burger.js
+++ src/js/components/burger.js
@@ -1,44 +1,53 @@
-// import { disableScroll } from '../functions/disable-scroll';
-// import { enableScroll } from '../functions/enable-scroll';
+import { disableScroll } from '../functions/disable-scroll';
+import { enableScroll } from '../functions/enable-scroll';
 
-// (function () {
-//   const burger = document.querySelector('[data-burger]');
-//   const menu = document.querySelector('[data-menu]');
-//   const menuItems = document.querySelectorAll('[data-menu-item]');
-//   const overlay = document.querySelector('[data-menu-overlay]');
+(function () {
+  const burger = document.querySelector('[data-burger]');
+  const menu = document.querySelector('[data-menu]');
+  const menuItems = document.querySelectorAll('[data-menu-item]');
+  const menuText = document.querySelector('[data-menu-text]');
 
-//   burger.addEventListener('click', (e) => {
-//     burger.classList.toggle('header__burger--active');
-//     menu.classList.toggle('header__nav--active');
-//     overlay.classList.toggle('overlay--active');
+  burger.addEventListener('click', (e) => {
+    burger.classList.toggle('header__burger--active');
+    menu.classList.toggle('header__nav--active');
 
-//     if (menu.classList.contains('header__nav--active')) {
-//       burger.setAttribute('aria-expanded', 'true');
-//       burger.setAttribute('aria-label', 'Close menu');
-//       disableScroll();
-//     } else {
-//       burger.setAttribute('aria-expanded', 'false');
-//       burger.setAttribute('aria-label', 'Open menu');
-//       enableScroll();
-//     }
-//   });
+    if (menu.classList.contains('header__nav--active')) {
+      burger.setAttribute('aria-expanded', 'true');
+      burger.setAttribute('aria-label', 'Close menu');
+      menuText.classList.add('is-hidden');
 
-//   overlay.addEventListener('click', () => {
-//     closeMenu();
-//   });
+      setTimeout(() => {
+        menuText.textContent = 'Close';
+        menuText.classList.remove('is-hidden');
+      }, 300);
 
-//   menuItems.forEach((el) => {
-//     el.addEventListener('click', () => {
-//       closeMenu();
-//     });
-//   });
+      disableScroll();
+    } else {
+      burger.setAttribute('aria-expanded', 'false');
+      burger.setAttribute('aria-label', 'Open menu');
+      menuText.classList.add('is-hidden');
 
-//   const closeMenu = () => {
-//     burger.setAttribute('aria-expanded', 'false');
-//     burger.setAttribute('aria-label', 'Open menu');
-//     burger.classList.remove('header__burger--active');
-//     menu.classList.remove('header__nav--active');
-//     overlay.classList.remove('overlay--active');
-//     enableScroll();
-//   };
-// })();
+      setTimeout(() => {
+        menuText.textContent = 'Menu';
+        menuText.classList.remove('is-hidden');
+      }, 300);
+
+      enableScroll();
+    }
+  });
+
+  menuItems.forEach((el) => {
+    el.addEventListener('click', () => {
+      closeMenu();
+    });
+  });
+
+  const closeMenu = () => {
+    burger.setAttribute('aria-expanded', 'false');
+    burger.setAttribute('aria-label', 'Open menu');
+    burger.classList.remove('header__burger--active');
+    menu.classList.remove('header__nav--active');
+    menuText.textContent = 'Menu';
+    enableScroll();
+  };
+})();
