43e70d17b08690fef05fc4766c30528d6cf909c7
Vetosy <xigga11@gmail.com>
2024-02-02 17:48:19 +0300

    fix

--- src/js/components/line-block-animation.js
+++ src/js/components/line-block-animation.js
@@ -1,7 +1,12 @@
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { MotionPathPlugin } from 'gsap/MotionPathPlugin';
+
 gsap.registerPlugin(ScrollTrigger, MotionPathPlugin);
+
+ScrollTrigger.defaults({
+  toggleActions: 'restart pause resume none',
+});
 
 window.addEventListener('load', () => {
   const titleSections = document.querySelectorAll('.title-section');
@@ -21,12 +26,17 @@ window.addEventListener('load', () => {
 
 const blockListItem = document.querySelectorAll('.block-list-line__item');
 
-const enterAnimateblockListItem = (item) => {
-  gsap.to(item, { opacity: 1, y: 0, duration: 0.6, delay: 0.3, ease: 'back.out(1.8)' });
-};
+const animateBlockListItem = (item, direction) => {
+  const opacityValue = direction === 1 ? 1 : 0;
+  const yValue = direction === 1 ? 0 : 100;
 
-const leaveAnimateblockListItem = (item) => {
-  gsap.to(item, { opacity: 0, y: 100, duration: 0.6, delay: 0.3, ease: 'back.out(1.8)' });
+  gsap.to(item, {
+    opacity: opacityValue,
+    y: yValue,
+    duration: 0.6,
+    delay: 0.3,
+    ease: 'back.out(1.8)',
+  });
 };
 
 blockListItem.forEach((blockItem) => {
@@ -34,11 +44,10 @@ blockListItem.forEach((blockItem) => {
     opacity: 0,
     scrollTrigger: {
       trigger: blockItem,
-      markers: true,
       start: 'top 65%',
       end: 'bottom top',
-      onEnter: () => enterAnimateblockListItem(blockItem),
-      onLeaveBack: () => leaveAnimateblockListItem(blockItem),
+      markers: true,
+      onToggle: ({ isActive }) => animateBlockListItem(blockItem, isActive ? 1 : -1),
     },
   });
 });
